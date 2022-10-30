072fc244e9ab9e57bb83a6e67c653ee96b5f4937
Андрей Кустов <andrey_kustov@192.168.1.5>
2022-10-30 20:13:00 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -5,19 +5,40 @@ from environs import Env
 import requests
 import telegram
 
+
+class LogsHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+
+        env = Env()
+        env.read_env()
+        tg_bot_token = env.str('TG_BOT_TOKEN')
+        tg_chat_id = env.int('TG_USER_CHAT_ID')
+        self.bot = telegram.Bot(token=tg_bot_token)
+        self.chat_id = tg_chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+
+        self.bot.send_message(
+            chat_id=self.chat_id,
+            text=log_entry
+        )
+
+        return log_entry
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('devman-bot')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(LogsHandler())
+
     env = Env()
     env.read_env()
 
-    tg_bot_token = env.str('TG_BOT_TOKEN')
-    logging.info('env variable TG_BOT_TOKEN has bean read')
-    tg_chat_id = env.int('TG_USER_CHAT_ID')
-    logging.info('env variable TG_USER_CHAT_ID has bean read')
     access_token = env.str('DEVMAN_ACCESS_TOKEN')
-    logging.info('env variable DEVMAN_ACCESS_TOKEN has bean read')
-
-    bot = telegram.Bot(token=tg_bot_token)
+    logger.info('env variable DEVMAN_ACCESS_TOKEN has bean read')
 
     headers = {'Authorization': f'Token {access_token}'}
     params = {}
@@ -45,10 +66,7 @@ if __name__ == '__main__':
             else:
                 verdict = 'Преподавателю все понравилось, можно приступать в слелующему уроку!'
 
-            bot.send_message(
-                chat_id=tg_chat_id,
-                text=f'У вас проверили работу "{lesson_title}"\n\n{verdict}'
-            )
+            logger.info(f'У вас проверили работу "{lesson_title}"\n\n{verdict}')
         elif review['status'] == 'timeout':
             timestamp_to_request = review['timestamp_to_request']
             params['timestamp'] = timestamp_to_request
